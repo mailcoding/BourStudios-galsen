fdb5fa084b415973337df25b437c711f5978fbd4
mailcoding <158386109+mailcoding@users.noreply.github.com>
2024-03-22 07:31:35 +0000

    ajout css resetPassword

--- galsen/urls.py
+++ galsen/urls.py
@@ -58,7 +58,7 @@ urlpatterns = [
     path('logout', log_out, name = 'logout'),
 
     # =========== reset password ================
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='auth/reset_password.html'), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm" ),
     path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
